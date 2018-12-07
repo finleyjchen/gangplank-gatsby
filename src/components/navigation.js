45d2a87c06c0f0813aa701a7d460295b26c4ecfa
Finley <finleyjchen@gmail.com>
2018-12-07 12:02:49 -0800

    navigation changes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link'
 import styles from './navigation.module.css'
 import logo from '../assets/bilgewater-logo-smaller.png'
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.logo}>
-        <Link to="/"><img src={logo} alt="logo" /></Link>
-      </li>
+  <nav role="navigation" className={styles.navigation}>
+    <span className={styles.logo}>
+      <Link to="/"><img src={logo} alt="logo" /></Link>
+    </span>
+    <ul className={styles.navigationItems}>
       <li className={styles.navigationItem}>
         <Link to="/blog/">Articles</Link>
       </li>
